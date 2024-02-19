c89e3f32d2dc699cc9e11d1112c1c4f46552f8c7
Ana Helena <anahc3@al.insper.edu.br>
2024-02-19 10:16:07 -0300

    feat: target collision + monkey collision

--- fase.py
+++ fase.py
@@ -15,13 +15,20 @@ class Fase:
         self.background = pygame.image.load(bg_path)
         self.background = pygame.transform.scale(self.background, (width, height))
         self.platform_rect = pygame.Rect(100, (self.height-80), 200, 40)
+        self.target_rect = pygame.Rect(700, (self.height-300), 60, 60)
         self.monkey = Monkey(200, self.platform_rect.top - 100)
         pygame.display.set_caption(title)
 
     def draw(self):
         self.screen.blit(self.background, (0, 0))
         pygame.draw.rect(self.screen, (0, 0, 255), self.platform_rect)
+        pygame.draw.rect(self.screen, (255, 0, 0), self.target_rect)
         self.monkey.draw()
 
     def update(self):
+        if self.monkey.monkey_rect.colliderect(self.target_rect):
+            self.monkey.reset()
+        if self.monkey.monkey_rect.colliderect(self.platform_rect):
+            self.monkey.on_platform = True
+            
         self.monkey.update()
--- monkey.py
+++ monkey.py
@@ -3,7 +3,6 @@ from pygame.locals import *
 import math
 import numpy as np
 
-# Inicia a classe Monkey
 class Monkey:
     def __init__(self, x, y, width = 1000, height = 500):
         self.initial_pos = np.array([x, y])
@@ -11,31 +10,42 @@ class Monkey:
         self.gravity = np.array([0, 0.03])
         self.screen = pygame.display.set_mode((width, height))
         self.image = pygame.transform.scale(pygame.image.load("assets\macaco.png"), (100, 100))
+        self.monkey_rect = self.image.get_rect()
         self.speed = self.initial_speed
         self.position = self.initial_pos
         self.mouse_clicked = False
+        self.on_platform = True
 
     def update(self):
-        
         if self.mouse_clicked:
             mouse_pos = np.array(pygame.mouse.get_pos())
             direction = mouse_pos - self.position
-            if np.linalg.norm(direction) != 0:  # Evitar divisão por zero
+            if np.linalg.norm(direction) != 0:  
                 direction = direction.astype(float) / np.linalg.norm(direction)
-            self.speed = direction * 5  # Velocidade ajustada na direção do mouse
-
-            self.position = self.position + self.speed
-            self.speed = self.speed + self.gravity
+            self.speed = direction * 5  
             self.mouse_clicked = False
+            self.on_platform = False
 
             if (self.position[1] < 0 or self.position[1] > 400) or (self.position[0] < 0 or self.position[0] > 900):
                 self.initial_speed = pygame.mouse.get_pos() - self.initial_pos
                 self.initial_speed = (self.initial_speed / np.linalg.norm(self.initial_speed)) * 5
                 self.speed = self.initial_speed
                 self.position = self.initial_pos
-            
-        self.position = self.position + self.speed
-        self.speed = self.speed + self.gravity
+        
+        if not self.on_platform:
+            self.speed = self.speed + self.gravity 
+            self.monkey_rect.topleft = self.position
+            self.position = self.position + self.speed
+
+            if self.position[1] > 500 or self.position[0] > 1000 or self.position[0] < 0 or self.position[1] < 0:   
+                self.reset()  
+
+    def reset(self):
+        self.position = self.initial_pos
+        self.speed = self.initial_speed
+        self.mouse_clicked = False
+        self.on_platform = True
 
     def draw(self):
-        self.screen.blit(self.image, self.position)
+        self.monkey_rect.topleft = self.position
+        self.screen.blit(self.image, self.monkey_rect)
