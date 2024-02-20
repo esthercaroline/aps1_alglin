ac5a29adfa7300b06b30f602fcfcfc56a39851a4
Ana Helena <anahc3@al.insper.edu.br>
2024-02-20 15:47:38 -0300

    feat: instructions screen

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
-from gerenciador_telas import Gerenciador_Telas
+from manage_screens import ManageScreen
 
 pygame.init()
 pygame.display.set_caption("Astro Ape")
 screen = pygame.display.set_mode((1000, 500))
-level = Gerenciador_Telas(screen)
+level = ManageScreen(screen)
 level.game_loop() 
