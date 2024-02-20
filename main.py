666050d8b81a278484d71ab0086e7d506f6ebb4a
Ana Helena <anahc3@al.insper.edu.br>
2024-02-20 16:58:46 -0300

    feat: music

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ from manage_screens import ManageScreen
 pygame.init()
 pygame.display.set_caption("Astro Ape")
 screen = pygame.display.set_mode((1000, 500))
+musica = pygame.mixer.music.load('assets\RoMak and the Space Pirates - No, Not Ever.mp3')
+pygame.mixer.music.play(loops = -1)
 level = ManageScreen(screen)
 level.game_loop() 
