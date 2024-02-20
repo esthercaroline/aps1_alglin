9ee5af45586e2bdb4b9e926104b996c14cb62dad
esthercaroline <esthercc@al.insper.edu.br>
2024-02-20 09:15:33 -0300

    feat: gravitational atractor

--- fase.py
+++ fase.py
@@ -9,15 +9,15 @@ pygame.init()
 class Fase:
     def __init__(self, width, height, title, bg_path):
         self.width = width
-        self.height = height 
+        self.height = height
         self.title = title
         self.screen = pygame.display.set_mode((width, height))
         self.background = pygame.image.load(bg_path)
         self.background = pygame.transform.scale(self.background, (width, height))
         self.platform_rect = pygame.Rect(100, (self.height-80), 200, 40)
         self.target_rect = pygame.Rect(700, (self.height-300), 60, 60)
-        self.banana= pygame.Rect(400, (self.height-400), 20, 60)
-        self.atraction_area = pygame.Rect(350, (self.height-450), 120, 160)
+        self.banana = pygame.Rect(400, (self.height-400), 20, 60)
+        self.constant_banana = 3000
         self.monkey = Monkey(200, self.platform_rect.top - 100)
         pygame.display.set_caption(title)
 
@@ -33,5 +33,5 @@ class Fase:
             self.monkey.reset()
         if self.monkey.monkey_rect.colliderect(self.platform_rect):
             self.monkey.on_platform = True
-            
-        self.monkey.update()
+
+        self.monkey.update(np.array([self.banana.x, self.banana.y]), self.constant_banana)
--- monkey.py
+++ monkey.py
@@ -4,9 +4,9 @@ import math
 import numpy as np
 
 class Monkey:
-    def __init__(self, x, y, width = 1000, height = 500):
+    def __init__(self, x, y, width=1000, height=500):
         self.initial_pos = np.array([x, y])
-        self.initial_speed = np.array([15, -10])
+        self.initial_speed = np.array([10, -10])
         self.gravity = np.array([0, 0.03])
         self.screen = pygame.display.set_mode((width, height))
         self.image = pygame.transform.scale(pygame.image.load("assets\macaco.png"), (100, 100))
@@ -16,13 +16,13 @@ class Monkey:
         self.mouse_clicked = False
         self.on_platform = True
 
-    def update(self):
+    def update(self, banana_pos, constant):
         if self.mouse_clicked and self.on_platform:
             mouse_pos = np.array(pygame.mouse.get_pos())
             direction = mouse_pos - self.position
-            if np.linalg.norm(direction) != 0:  
+            if np.linalg.norm(direction) != 0:
                 direction = direction.astype(float) / np.linalg.norm(direction)
-            self.speed = direction * 5  
+            self.speed = direction * 5
             self.mouse_clicked = False
             self.on_platform = False
 
@@ -31,14 +31,21 @@ class Monkey:
                 self.initial_speed = (self.initial_speed / np.linalg.norm(self.initial_speed)) * 5
                 self.speed = self.initial_speed
                 self.position = self.initial_pos
-        
+
         if not self.on_platform:
-            self.speed = self.speed + self.gravity 
+            # Calculating attraction towards banana
+            direction_to_banana = banana_pos - self.position
+            distance_to_banana = np.linalg.norm(direction_to_banana)
+            if distance_to_banana != 0:
+                attraction_force = (constant / distance_to_banana**2) * (direction_to_banana / distance_to_banana)
+                self.speed += attraction_force
+
+            self.speed += self.gravity
             self.monkey_rect.topleft = self.position
             self.position = self.position + self.speed
 
-            if self.position[1] > 500 or self.position[0] > 1000 or self.position[0] < 0 or self.position[1] < 0:   
-                self.reset()  
+            if self.position[1] > 500 or self.position[0] > 1000 or self.position[0] < 0 or self.position[1] < 0:
+                self.reset()
 
     def reset(self):
         self.position = self.initial_pos
