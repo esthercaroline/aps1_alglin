ea61e72697713b84ea55b584067efba36aa4952a
Ana Helena <anahc3@al.insper.edu.br>
2024-02-23 00:04:14 -0300

    fix: minor code changes

--- telas_meio.py
+++ telas_meio.py
@@ -6,10 +6,10 @@ class TelaMeio1():
     def __init__(self):
         # Initialize the game
         pygame.init()
-        self.tempo_passado = 0
-        self.tempo_total = 200000
+        self.time_passed = 0
+        self.total_time = 200000
         self.screen = pygame.display.set_mode((1000, 500))
-        self.texto = "Nível 1"
+        self.text = "Nível 1"
 
     def update(self):
         # Event handling and screen update
@@ -18,67 +18,64 @@ class TelaMeio1():
                 return -1
 
         # Update the time passed
-        self.tempo_passado += pygame.time.get_ticks()
-        if self.tempo_passado >= self.tempo_total:
+        self.time_passed += pygame.time.get_ticks()
+        if self.time_passed >= self.total_time:
             return "GAME_SCREEN"
 
     def draw(self):
-        imagem_fundo = pygame.image.load('assets/bg_space.png')
-        imagem_fundo = pygame.transform.scale(imagem_fundo, (1000, 500))
-        fonte = pygame.font.Font("assets/BruceForeverRegular-X3jd2.ttf", 100)  
-        texto = fonte.render(self.texto, True, (255,255,255))
-        self.screen.blit(imagem_fundo, (0, 0))
-        self.screen.blit(texto, texto.get_rect(center=(1000/2, 250)))
+        bg_img = pygame.image.load('assets/bg_space.png')
+        bg_img = pygame.transform.scale(bg_img, (1000, 500))
+        font = pygame.font.Font("assets/BruceForeverRegular-X3jd2.ttf", 100)  
+        text = font.render(self.text, True, (255,255,255))
+        self.screen.blit(bg_img, (0, 0))
+        self.screen.blit(text, text.get_rect(center=(1000/2, 250)))
 
 class TelaMeio2():
     def __init__(self):
         pygame.init()
-        self.tempo_passado = 0
-        self.tempo_total = 200000
+        self.time_passed = 0
+        self.total_time = 200000
         self.screen = pygame.display.set_mode((1000, 500))
-        self.texto = "Nível 2"
+        self.text = "Nível 2"
 
     def update(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return -1
 
-        self.tempo_passado += pygame.time.get_ticks()
-        if self.tempo_passado >= self.tempo_total:
+        self.time_passed += pygame.time.get_ticks()
+        if self.time_passed >= self.total_time:
             return "FASE2"
 
     def draw(self):
-        imagem_fundo = pygame.image.load('assets\orange_bg.png')
-        imagem_fundo = pygame.transform.scale(imagem_fundo, (1000, 500))
-        fonte = pygame.font.Font("assets/BruceForeverRegular-X3jd2.ttf", 100)  # Corrigido o caminho da fonte
-        texto = fonte.render(self.texto, True, (255, 255, 255))
-        # Usando blit para desenhar o texto na tela
-        self.screen.blit(imagem_fundo, (0, 0))
-        self.screen.blit(texto, texto.get_rect(center=(1000/2, 250)))
+        bg_img = pygame.image.load('assets\orange_bg.png')
+        bg_img = pygame.transform.scale(bg_img, (1000, 500))
+        font = pygame.font.Font("assets/BruceForeverRegular-X3jd2.ttf", 100)  
+        text = font.render(self.text, True, (255, 255, 255))
+        self.screen.blit(bg_img, (0, 0))
+        self.screen.blit(text, text.get_rect(center=(1000/2, 250)))
 
 class TelaMeio3():
-
     def __init__(self):
         pygame.init()
-        self.tempo_passado = 0
-        self.tempo_total = 200000
+        self.time_passed = 0
+        self.total_time = 200000
         self.screen = pygame.display.set_mode((1000, 500))
-        self.texto = "Nível 3"
+        self.text = "Nível 3"
 
     def update(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return -1
 
-        self.tempo_passado += pygame.time.get_ticks()
-        if self.tempo_passado >= self.tempo_total:
+        self.time_passed += pygame.time.get_ticks()
+        if self.time_passed >= self.total_time:
             return "FASE3"
 
     def draw(self):
-        imagem_fundo = pygame.image.load('assets/green_bg.png')
-        imagem_fundo = pygame.transform.scale(imagem_fundo, (1000, 500))
-        fonte = pygame.font.Font("assets/BruceForeverRegular-X3jd2.ttf", 100)  # Corrigido o caminho da fonte
-        texto = fonte.render(self.texto, True, (255, 255, 255))
-        # Usando blit para desenhar o texto na tela
-        self.screen.blit(imagem_fundo, (0, 0))
-        self.screen.blit(texto, texto.get_rect(center=(1000/2, 250)))
+        bg_img = pygame.image.load('assets/green_bg.png')
+        bg_img = pygame.transform.scale(bg_img, (1000, 500))
+        font = pygame.font.Font("assets/BruceForeverRegular-X3jd2.ttf", 100) 
+        text = font.render(self.texto, True, (255, 255, 255))
+        self.screen.blit(bg_img, (0, 0))
+        self.screen.blit(text, text.get_rect(center=(1000/2, 250)))
